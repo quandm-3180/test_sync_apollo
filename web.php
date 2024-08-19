5a65da6256b962b461c7198225c7c523f5808c17
DoMinhQuan <111850158+quandm-3180@users.noreply.github.com>
2024-08-19 16:14:28 +0700

    Delete web.php

--- web.php
+++
@@ -1,9 +0,0 @@
-<?php
-Route::group([
-    'prefix' => '/profdffile',
-    'namespace' => 'Profffsdsile',
-    'middleware' => 'unitfdsffdsffdsfds-managfdser',
-], function () {
-    Route::get('code-changfsdse-by-language', 'GetCodeChfdsfdangeByLanguage');
-    Route::get('fds', 'GetActivifdsfdfties');
-});
