d336e436c676866337ee82ee8d6528c66195a375
DoMinhQuan <111850158+quandm-3180@users.noreply.github.com>
2024-08-19 16:10:38 +0700

    Create web.php

---
+++ web.php
@@ -0,0 +1,9 @@
+<?php
+Route::group([
+    'prefix' => '/profdffile',
+    'namespace' => 'Profffsdsile',
+    'middleware' => 'unitfdsffdsffdsfds-managfdser',
+], function () {
+    Route::get('code-changfsdse-by-language', 'GetCodeChfdsfdangeByLanguage');
+    Route::get('fds', 'GetActivifdsfdfties');
+});
